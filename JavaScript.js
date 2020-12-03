06482d328b957e113ebd2b0d338ca75152cdc6bb
Korey <korey.hayes@flemingcollege.ca>
2020-12-02 21:10:00 -0500

    click drop downs

--- JavaScript.js
+++ JavaScript.js
@@ -42,4 +42,30 @@ function validate(){
     alert("Form Submitted Successfully!");
     return true;
   }
-//Kedie  
+//Kedie
+
+// Javascript Owner: Korey Hayes
+// Source: w3schools
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function clickDrop() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.topnav')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
